e51b149117c5b96a2336c8b5d4956539438b3586
andrei.fernandes <andreifernandezdevv@gmail.com>
2026-01-21 13:58:39 -0300

    feat: implement User CRUD with global exception handling
    Added insert, delete and update methods to UserResource.
    
    Implemented custom exception handling for FindById, Delete and Update.
    
    Added ResourceNotFoundException and DatabaseException handlers.
    
    Configured StandardError and ResourceExceptionHandler.

---
+++ src/main/java/com/fernandez/course/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.fernandez.course.resources.exception;
+
+
+import com.fernandez.course.services.exceptions.DatabaseException;
+import com.fernandez.course.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+
+          String error = "Resource not found";
+          HttpStatus status = HttpStatus.NOT_FOUND;
+          StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+          return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/com/fernandez/course/services/UserService.java
+++ src/main/java/com/fernandez/course/services/UserService.java
@@ -3,7 +3,12 @@ package com.fernandez.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.fernandez.course.services.exceptions.DatabaseException;
+import com.fernandez.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.fernandez.course.User;
@@ -22,10 +27,38 @@ public class UserService {
     
     public User findById(Long id) {
     	Optional <User> obj = repository.findById(id);
-    	return obj.get();
+    	return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
 	public User insert(User obj){
 		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public User update(Long id, User obj){
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
